44303d557bb3d4070358d129b5d1a7e3c110b98a
vs-david-su <david.ti.su@viewsonic.com>
2026-04-20 18:13:40 +0800

    test

--- badcode.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def login(username, password):
-    conn = sqlite3.connect('test.db')
-    cursor = conn.cursor()
-    # 這是要在這個 PR 裡新增的危險代碼
-    query = f"SELECT * FROM users WHERE user='{username}' AND pass='{password}'"
-    cursor.execute(query)
-    return cursor.fetchall()
---
+++ test_Vuln.js
@@ -0,0 +1,9 @@
+// test_vuln.js
+function runCode(userInput) {
+    // 這是經典的程式碼注入漏洞
+    // ESLint 規則：no-eval
+    var result = eval(userInput);
+    console.log(result);
+}
+
+runCode("alert('hacked')");
